8b11f900c33606b1e29d52994fc8d7cf11b29c7c
Alexander Jack <jackx022@umn.edu>
2022-11-16 13:24:15 -0600

    remove some temporary variables

--- OneDrive/Desktop/cwd-project-2-master/test.r
+++ OneDrive/Desktop/cwd-project-2-master/test.r
@@ -103,12 +103,19 @@ getClosestElementIndex<-function(nbrs, selection){
 
 makeDecision<-function(ind, lc.stck, lc){
   selection<-NA
+  weights<-c()
+  landscape.vec<-c()
+  
   nbrs<-adjacent(lc.stck[[1]],ind$crnt.cell,directions=case,pairs=FALSE) 
   # check if centroid is in the nbrs list
   if(!ind$cell.init %in% nbrs){
     nbrs[length(nbrs) + 1] <- ind$cell.init
   }
   nbrs.ext<-extentFromCells(lc.stck[[1]], nbrs)
+  # TODO subset nbrs to be based on bearing
+  # It's not biologically realistic to have an individual sample from 
+  # a 360 degree radius at every time step. Just sample in the direction of
+  # movement
   nbrs<-cellsFromExtent(lc.stck[[1]], nbrs.ext)
   nbrs<-nbrs[!is.na(nbrs)]# make sure we aren't on an edge
   nbrs.ext<-extentFromCells(lc.stck[[1]],nbrs)
@@ -118,36 +125,28 @@ makeDecision<-function(ind, lc.stck, lc){
   lc.hr[1:length(lc.hr)]<-NA
   lc.hr[ind$cell.init]<-1
   # MAKE SOME DIRECTED MOVEMENT
+  # this can eventually be replaced with a case statement
+  # the individual will have movement states
   if(ind$is.thirsty){
     dist.to.water<-distance(lc.stck[[3]])
     if(dist.to.water[ind$crnt.cell]<=20){
       ind$is.thirsty<-FALSE
       ind$last.drink<-0
       dir.hr<-direction(lc.hr)
-      dir.to.hr<-dir.hr[ind$crnt.cell]
-      selection<-ind$cell.init + dir.to.hr
-      selection.index<-getClosestElementIndex(nbrs, selection)
+      selection<-ind$cell.init + dir.hr[ind$crnt.cell]
       weights<-rep.int(1,length(nbrs))
-      weights[selection.index]<-2
-      selection<-sample(sample(c(1:length(nbrs)),size=100,replace=TRUE, prob=weights),1)
-      selection<-nbrs[selection]
+      weights[getClosestElementIndex(nbrs, selection)]<-2
+      selection<-nbrs[sample(sample(c(1:length(nbrs)),size=100,replace=TRUE, prob=weights),1)]
     }
     else{
       dir.to.water<-direction(lc.stck[[3]])
       selection<-ind$crnt.cell + dir.to.water[ind$crnt.cell]
-      selection.index<-getClosestElementIndex(nbrs, selection)
       weights<-rep.int(1,length(nbrs))
-      weights[selection.index]<-2
-      selection<-sample(sample(c(1:length(nbrs)),size=100,replace=TRUE, prob=weights),1)
-      selection<-nbrs[selection]
+      weights[getClosestElementIndex(nbrs, selection)]<-2
+      selection<-nbrs[sample(sample(c(1:length(nbrs)),size=100,replace=TRUE, prob=weights),1)]
     }
   }
   else{
-  weights<-c()
-  landscape.vec<-c()
- 
-  
-  
   for(i in 1:length(nbrs)){
     nbr.ext<-extentFromCells(lc.stck[[1]], nbrs[i])
     cells<-cellsFromExtent(lc, nbr.ext)
@@ -159,8 +158,7 @@ makeDecision<-function(ind, lc.stck, lc){
   if(distance(lc.hr)[ind$crnt.cell] >= 400){
     weights<-landscape.vec*nbr.crop.dis
   }
-  weighted.sel<-sample(sample(c(1:length(nbrs)), size=100, replace=TRUE, prob=weights),1)
-  selection<-nbrs[weighted.sel]
+  selection<-nbrs[sample(sample(c(1:length(nbrs)), size=100, replace=TRUE, prob=weights),1)]
   }
   selection
 }
@@ -183,44 +181,40 @@ infctn[1:length(infctn)]<-0
 lc.water<-lc.agg==5
 lc.water[lc.water==0]<-NA # so we can utilize distance function
 lc.stck<-stack(lc.agg, infctn, lc.water)
-inds<-make_inds(3,
-          xmin(lc.agg),
-          xmax(lc.agg),
-          ymin(lc.agg),
-          ymax(lc.agg),
-          1,
-          lc.agg)
-write.csv(inds, "C:\\Users\\jackx\\Desktop\\deerdat.csv", row.names=FALSE)
-startTime=Sys.time()
-for(t in 1:20){
-  for(i in 1:nrow(inds)){
-    thirst.weights<-c(inds[i,]$thirst.threshold, inds[i,]$last.drink)
-    inds[i,]$is.thirsty<-sample(sample(c(FALSE, TRUE), size=100, replace=TRUE, prob=thirst.weights),1)
-    if(inds[i,]$status=="I"){
-      lc.stck[[2]][inds[i,]$crnt.cell] <- lc.stck[[2]][inds[i,]$crnt.cell] + 1
-    # check if the current cell is occupied by another individual
-    for(j in 1:nrow(inds)){
-
-      if((inds[i,]$crnt.cell == inds[j,]$crnt.cell) & !(inds[i,]$id == inds[j,]$id)){
-        inds[j,]$status = "I"
-      }
-    }
-  }
-  inds[i,]$crnt.cell<-move(inds[i,], lc.stck, lc) # grab the new cell
-  print(paste("this is current cell:", inds[i,]$crnt.cell, sep=" "))
-  new.coord<-xyFromCell(lc.stck, inds[i,]$crnt.cell)
-  inds[i,]$x <- new.coord[1]
-  inds[i,]$y <- new.coord[2]
-  inds[i,]$last.drink=inds[i,]$last.drink+1
-  inds[i,]$time_step = inds[1,]$time_step+1
-  }
-  write.table(inds,  "C:\\Users\\jackx\\Desktop\\deerdat.csv",
-              row.names=FALSE, sep=",", append=TRUE, col.names=FALSE)
-  #print(inds)
-  
-}
-endTime=Sys.time()
-print(paste("total run time: ", endTime-startTime, sep=""))
+# inds<-make_inds(50,
+#           xmin(lc.agg),
+#           xmax(lc.agg),
+#           ymin(lc.agg),
+#           ymax(lc.agg),
+#           1,
+#           lc.agg)
+# write.csv(inds, "C:\\Users\\jackx\\Desktop\\deerdat.csv", row.names=FALSE)
+# startTime=Sys.time()
+# for(t in 1:40){
+#   for(i in 1:nrow(inds)){
+#     thirst.weights<-c(inds[i,]$thirst.threshold, inds[i,]$last.drink)
+#     inds[i,]$is.thirsty<-sample(sample(c(FALSE, TRUE), size=100, replace=TRUE, prob=thirst.weights),1)
+#     if(inds[i,]$status=="I"){
+#       lc.stck[[2]][inds[i,]$crnt.cell] <- lc.stck[[2]][inds[i,]$crnt.cell] + 1
+#     # check if the current cell is occupied by another individual
+#     for(j in 1:nrow(inds)){
+#       if((inds[i,]$crnt.cell == inds[j,]$crnt.cell) & !(inds[i,]$id == inds[j,]$id)){
+#         inds[j,]$status = "I"
+#       }
+#     }
+#   }
+#   inds[i,]$crnt.cell<-move(inds[i,], lc.stck, lc) # grab the new cell
+#   new.coord<-xyFromCell(lc.stck, inds[i,]$crnt.cell)
+#   inds[i,]$x <- new.coord[1]
+#   inds[i,]$y <- new.coord[2]
+#   inds[i,]$last.drink=inds[i,]$last.drink+1
+#   inds[i,]$time_step = inds[1,]$time_step+1
+#   }
+#   write.table(inds,  "C:\\Users\\jackx\\Desktop\\deerdat.csv",
+#               row.names=FALSE, sep=",", append=TRUE, col.names=FALSE)
+# }
+# endTime=Sys.time()
+# print(paste("total run time: ", endTime-startTime, sep=""))
 data<-read.csv("C:\\Users\\jackx\\Desktop\\deerdat.csv")
 lc.pts <- rasterToPoints(lc.stck[[1]], spatial = TRUE)
 lc.df  <- data.frame(lc.pts)
