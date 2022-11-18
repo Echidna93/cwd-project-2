bafbb886f85c47c73361a9c2b835779e74a7d0b7
Alexander Jack <jackx022@umn.edu>
2022-11-18 16:27:01 -0600

    fixes

--- OneDrive/Desktop/cwd-project-2-master/test.r
+++ OneDrive/Desktop/cwd-project-2-master/test.r
@@ -71,7 +71,7 @@ make_inds <- function(n.initial,
   y.init <- y
   is.thirsty <- rep(c(FALSE), n.initial)
   last.drink <- rep(c(0), n.initial)
-  thirst.threshold<-rep(c(1:6), n.initial)
+  thirst.threshold<-sample(c(1:6),50, replace=TRUE)
   I<-sample(1:n.initial, nI)
   status<-rep("S", times=n.initial)
   status[I]<-"I"
@@ -88,9 +88,6 @@ make_inds <- function(n.initial,
              thirst.threshold=thirst.threshold,
              time_step=1,
              stringsAsFactors=FALSE)
-}
-
-getLandscapeMetric<-function(nbr){
 }
 
 getClosestElementIndex<-function(nbrs, selection){
@@ -154,10 +151,7 @@ makeDecision<-function(ind, lc.stck, lc){
   if(distance(lc.hr)[ind$crnt.cell] >= 400){
     weights<-landscape.vec*nbr.crop.dis
   }
-  selection<-nbrs[sample(sample(c(1:length(nbrs)),
-                                size=100,
-                                replace=TRUE,
-                                prob=weights),1)]
+  selection<-nbrs[sample(sample(c(1:length(nbrs)), size=100, replace=TRUE, prob=weights),1)]
   }
   selection
 }
@@ -179,44 +173,50 @@ infctn[1:length(infctn)]<-0
 lc.water<-lc.agg==5
 lc.water[lc.water==0]<-NA # so we can utilize distance function
 lc.stck<-stack(lc.agg, infctn, lc.water)
-inds<-make_inds(50,
-          xmin(lc.agg),
-          xmax(lc.agg),
-          ymin(lc.agg),
-          ymax(lc.agg),
-          1,
-          lc.agg)
-write.csv(inds, "C:\\Users\\jackx\\Desktop\\deerdat.csv", row.names=FALSE)
-startTime=Sys.time()
-for(t in 1:40){
-  for(i in 1:nrow(inds)){
-    thirst.weights<-c(inds[i,]$thirst.threshold, inds[i,]$last.drink)
-    inds[i,]$is.thirsty<-sample(sample(c(FALSE, TRUE), size=100, replace=TRUE, prob=thirst.weights),1)
-    if(inds[i,]$status=="I"){
-      lc.stck[[2]][inds[i,]$crnt.cell] <- lc.stck[[2]][inds[i,]$crnt.cell] + 1
-    # check if the current cell is occupied by another individual
-    for(j in 1:nrow(inds)){
-      if((inds[i,]$crnt.cell == inds[j,]$crnt.cell) & !(inds[i,]$id == inds[j,]$id)){
-        inds[j,]$status = "I"
-      }
-    }
-  }
-  inds[i,]$crnt.cell<-move(inds[i,], lc.stck, lc) # grab the new cell
-  new.coord<-xyFromCell(lc.stck, inds[i,]$crnt.cell)
-  inds[i,]$x <- new.coord[1]
-  inds[i,]$y <- new.coord[2]
-  inds[i,]$last.drink=inds[i,]$last.drink+1
-  inds[i,]$time_step = inds[1,]$time_step+1
-  }
-  write.table(inds,
-              "C:\\Users\\jackx\\Desktop\\deerdat.csv",
-              row.names=FALSE,
-              sep=",",
-              append=TRUE,
-              col.names=FALSE)
-}
-endTime=Sys.time()
-print(paste("total run time: ", endTime-startTime, sep=""))
+# inds<-make_inds(50,
+#           xmin(lc.agg),
+#           xmax(lc.agg),
+#           ymin(lc.agg),
+#           ymax(lc.agg),
+#           1,
+#           lc.agg)
+# 
+# write.csv(inds, "C:\\Users\\jackx\\Desktop\\deerdat.csv", row.names=FALSE)
+# #startTime=Sys.time()
+# for(t in 1:40){
+#   startTime=Sys.time()
+#   for(i in 1:nrow(inds)){
+#     thirst.weights<-c(inds[i,]$thirst.threshold, inds[i,]$last.drink)
+#     inds[i,]$is.thirsty<-sample(sample(c(FALSE, TRUE), size=100, replace=TRUE, prob=thirst.weights),1)
+#     if(inds[i,]$status=="I"){
+#       lc.stck[[2]][inds[i,]$crnt.cell] <- lc.stck[[2]][inds[i,]$crnt.cell] + 1
+#     # check if the current cell is occupied by another individual
+#     for(j in 1:nrow(inds)){
+#       if((inds[i,]$crnt.cell == inds[j,]$crnt.cell) & !(inds[i,]$id == inds[j,]$id)){
+#         inds[j,]$status = "I"
+#       }
+#     }
+#     }
+#   
+#   inds[i,]$crnt.cell<-move(inds[i,], lc.stck, lc) # grab the new cell
+#   new.coord<-xyFromCell(lc.stck, inds[i,]$crnt.cell)
+#   inds[i,]$x <- new.coord[1]
+#   inds[i,]$y <- new.coord[2]
+#   inds[i,]$last.drink=inds[i,]$last.drink+1
+#   inds[i,]$time_step = inds[1,]$time_step+1
+#   }
+#   write.table(inds,
+#               "C:\\Users\\jackx\\Desktop\\deerdat.csv",
+#               row.names=FALSE,
+#               sep=",",
+#               append=TRUE,
+#               col.names=FALSE)
+#   endTime=Sys.time()
+#   print(paste("time step ", t, " run time: ", endTime-startTime, sep=""))
+#   #print(paste("time step: ", t, sep = ""))
+# }
+#endTime=Sys.time()
+#print(paste("total run time: ", endTime-startTime, sep=""))
 data<-read.csv("C:\\Users\\jackx\\Desktop\\deerdat.csv")
 lc.pts <- rasterToPoints(lc.stck[[1]], spatial = TRUE)
 lc.df  <- data.frame(lc.pts)
